cb08d4b4f04e253d571c81d0251e502eb5da27a7
Kopila Devkota <kopiladevkota7@gmail.com>
2026-03-12 08:39:24 +0545

    initial

---
+++ nepal_utility_system_menu.py
@@ -0,0 +1,35 @@
+# Menu Driven Program for Repository Links
+
+while True:
+    print("\n----- Repository Menu -----")
+    print("1. BMI Calculator")
+    print("2. Taxation System")
+    print("3. Currency COnverter")
+    print("4. Remittance")
+    print("5. Lagani Kosh")
+    print("6. Invalid")
+
+    choice = int(input("Enter your choice (1-6): "))
+
+    if choice == 1:
+        print("BMI Calculator link: https://github.com/bidhyabhattarai/BMI_calculator")
+
+    elif choice == 2:
+        print("Taxation System link: https://github.com/Anujakhatri/Taxation-system-nepal")
+
+    elif choice == 3:
+        print("Currency COnverter link: https://github.com/sneharitas/currency_converter")
+
+    elif choice == 4:
+        print("Remittance link: https://github.com/binisha77/nepal_remittance_calculator")
+
+    elif choice == 5:
+        print("Lagani Kosh link: https://github.com/kopiladevkota/nepal_lagani_kosh")
+
+    elif choice == 6:
+        print("Invalid choice. Please select 1-6.")
+       
+
+    else:
+        print("Exiting program...")
+        break
